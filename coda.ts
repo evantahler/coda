c4fbf0865fac9ccbd3d9df78b3bce48fe9be02ba
Evan Tahler <evan@evantahler.com>
2025-06-15 14:17:59 -0700

    memory scaffold

--- coda.ts
+++ coda.ts
@@ -57,4 +57,103 @@ program
     process.exit(0);
   });
 
+const memoryCommand = program
+  .command("memory")
+  .description("Manage memory entries");
+
+memoryCommand
+  .command("list")
+  .description("List all memory entries")
+  .option(
+    "-d, --directory [directory]",
+    "The path to the directory to analyze",
+    process.cwd(),
+  )
+  .option(
+    "-l, --log_level [level]",
+    "The log level.  Options are: debug, info, warn, error",
+    process.env.LOG_LEVEL,
+  )
+  .option(
+    "-c, --colorize [colorize]",
+    "Colorize the log output",
+    process.env.LOG_COLOR,
+  )
+  .option(
+    "-t, --timestamps [timestamps]",
+    "Include timestamps in the log output",
+    process.env.LOG_TIMESTAMPS,
+  )
+  .action(async (options) => {
+    const config = new Config(options);
+    const logger = new Logger(config);
+    // TODO: Implement memory listing logic
+    logger.info("Listing memory entries...");
+    process.exit(0);
+  });
+
+memoryCommand
+  .command("add")
+  .description("Add a new memory entry")
+  .option(
+    "-d, --directory [directory]",
+    "The path to the directory to analyze",
+    process.cwd(),
+  )
+  .option(
+    "-l, --log_level [level]",
+    "The log level.  Options are: debug, info, warn, error",
+    process.env.LOG_LEVEL,
+  )
+  .option(
+    "-c, --colorize [colorize]",
+    "Colorize the log output",
+    process.env.LOG_COLOR,
+  )
+  .option(
+    "-t, --timestamps [timestamps]",
+    "Include timestamps in the log output",
+    process.env.LOG_TIMESTAMPS,
+  )
+  .argument("<content>", "The content to add to memory")
+  .action(async (content, options) => {
+    const config = new Config(options);
+    const logger = new Logger(config);
+    // TODO: Implement memory addition logic
+    logger.info(`Adding memory entry: ${content}`);
+    process.exit(0);
+  });
+
+memoryCommand
+  .command("remove")
+  .description("Remove a memory entry")
+  .option(
+    "-d, --directory [directory]",
+    "The path to the directory to analyze",
+    process.cwd(),
+  )
+  .option(
+    "-l, --log_level [level]",
+    "The log level.  Options are: debug, info, warn, error",
+    process.env.LOG_LEVEL,
+  )
+  .option(
+    "-c, --colorize [colorize]",
+    "Colorize the log output",
+    process.env.LOG_COLOR,
+  )
+  .option(
+    "-t, --timestamps [timestamps]",
+    "Include timestamps in the log output",
+    process.env.LOG_TIMESTAMPS,
+  )
+  .argument("<id>", "The ID of the memory entry to remove")
+  .action(async (id, options) => {
+    const config = new Config(options);
+    const logger = new Logger(config);
+    // TODO: Implement memory removal logic
+    logger.info(`Removing memory entry with ID: ${id}`);
+    process.exit(0);
+  });
+
 program.parse();
